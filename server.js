0a6acc890c981639ecdcd09302e3f436c701234f
Gerihh <horvathgergo1208@gmail.com>
2023-11-14 09:38:22 +0100

    routes

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const trainings = require("./routes/trainings");
+const morgan = require('morgan')
+
 require("dotenv").config(); // A .env fájlt olvassa
 const app = express();
+
+app.use(morgan('dev'));
+
+app.use("/api/trainings", trainings);
+
 app.listen(
   process.env.PORT,
   console.log(`Server running on port ${process.env.PORT}`)
