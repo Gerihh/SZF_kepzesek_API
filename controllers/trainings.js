03e05df91436fca956a832b3df5af686be43c4ea
Gerihh <horvathgergo1208@gmail.com>
2023-11-21 09:06:54 +0100

    POST kérés

--- controllers/trainings.js
+++ controllers/trainings.js
@@ -1,8 +1,11 @@
+const Training = require('../models/Training')
 // @desc   Get all trainings
 // @route  GET /api/trainings
 // @access Public
 exports.getTrainings = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Show all trainings", hello: req.hello });
+  res
+    .status(200)
+    .json({ success: true, msg: "Show all trainings", hello: req.hello });
 }; // @desc   Get single training
 // @route  GET /api/trainings/:id
 // @access Public
@@ -11,10 +14,15 @@ exports.getTraining = (req, res, next) => {
 }; // @desc   Create new training
 // @route  POST /api/trainings
 // @access Private
-exports.createTraining = (req, res, next) => {
-  console.log(req.body);
-  res.status(200).json({ success: true, msg: "Create new training" });
-}; // @desc   Update training
+exports.createTraining = async (req, res, next) => {
+  try {
+    const training = await Training.create(req.body);
+    res.status(201).json({ success: true, data: training });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
+};
+// @desc   Update training
 // @route  PUT /api/trainings/:id
 // @access Private
 exports.updateTraining = (req, res, next) => {
